560a2faa54834f45b19a44ef6ab735bd60d9fc7a
luu <trinhluu33@gmail.com>
2018-06-14 18:50:55 +0700

    initial project

---
+++ styles/common.js
@@ -0,0 +1,6 @@
+export const COLOR_PRIMARY = '#965AF0';
+export const COLOR_SECONDARY = '#612bbc';
+export const BACKGROUND_COLOR = '#F5FCFF';
+export const FONT_NORMAL = 'OpenSans-Regular';
+export const FONT_BOLD = 'OpenSans-Bold';
+export const BORDER_RADIUS = 5;
---
+++ styles/styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        color: 'blue',
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+    smalltext: {
+        color: 'gray',
+        fontSize: 14
+    },
+    mediumText: {
+        color: 'gray',
+        fontSize: 16
+    },
+    bold: {
+        fontWeight: 'bold'
+    },
+});
